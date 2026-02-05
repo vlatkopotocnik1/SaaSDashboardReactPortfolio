f3c48d16a4b3f2abec46a26695faf8364b153a19
vlatk <vlatk@DESKTOP-0KIH4S4>
2026-02-05 15:05:54 +0100

    Refactor pre-commit hook and reformat client code

--- saasdashboard.client/CHANGELOG.md
+++ saasdashboard.client/CHANGELOG.md
@@ -1,9 +1,11 @@
 This file explains how Visual Studio created the project.
 
 The following tools were used to generate this project:
+
 - create-vite
 
 The following steps were used to generate this project:
+
 - Create react project with create-vite: `npm init --yes vite@latest saasdashboard.client -- --template=react-ts`.
 - Create project file (`saasdashboard.client.esproj`).
 - Create `launch.json` to enable debugging.
--- saasdashboard.client/eslint.config.js
+++ saasdashboard.client/eslint.config.js
@@ -1,10 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import prettier from 'eslint-config-prettier'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -22,4 +22,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+]);
